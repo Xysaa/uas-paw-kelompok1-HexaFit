d376717ae8ab860359378c082e8f6f5019f08f8f
Stevanus Cahya Anggara <stevanuschyaanggara@gmail.com>
2025-12-19 07:15:44 +0700

    test

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: false,
   timeout: 15000,
   headers: {
     "Content-Type": "application/json",
